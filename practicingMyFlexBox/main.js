fee392ddc8b80e0b6b16a005b5d2b18af3f9f691
devVGBG <resourcefulmindoso@gmail.com>
2022-02-28 18:44:32 +0100

    modify flexbox practice and add javascript for a cupid message display

---
+++ practicingMyFlexBox/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#tell-her-something').addEventListener('click', checkClick)
+
+function checkClick() {
+    document.querySelector('#display-message').innerHTML = 'Just like the sun lights up the earth, your smile brightens my day. Good morning, Ohema! I also wanted to let you know that my lonely heart is counting every sunrise and waiting for the day we’ll meet again. Until then, have a lovely day!';
+    document.querySelector('#display-message-one').innerHTML = 'Though I may not see you as much as I would love to, I may not have you in my arms as often as I would want, but deep in my heart I know for a fact, you are the only one for me and I will not let you go.';
+    document.querySelector('#display-message-two').innerHTML = 'Bite me Javascript';
+}
+
